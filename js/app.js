92b05271fa0fe4acc36b06748a88b3c0963179c1
Doug Wight <dcfwight@gmail.com>
2017-11-10 23:42:54 +0000

    HUGE changes - moved script code from index.html to app.js. Also moved more of the code for markers etc into the Place object, so that they can be responsive. It is still a big mess - need to transfer more over

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-//------------Initial data for markers---------------------------------------//
+//------------Initial data for places---------------------------------------//
 var places = [
 				{title: "Edinburgh Castle",
 				location: {lat: 55.9485947,
@@ -13,23 +13,302 @@ var places = [
 				location: {lat: 55.9821554,
 							lng: -3.1772521 }
 				}
-				
 			];
-var Marker = function(data) {
-	this.title=ko.observable(data.title);
-	this.location=ko.observable(data.location);
+
+var map;
+
+var Place = function(data) {
+	var self = this;
+	self.title=ko.observable(data.title);
+	self.location=data.location;
+	self.clicked = function() {
+		console.log(self.title() + ' clicked');
+		console.log("selected: " + self.selected());
+	};
+	self.selected=ko.observable(false);
+	self.visible=ko.observable(true);
+	self.description = self.title() + ", Edinburgh";
+	self.infoWindow = new google.maps.InfoWindow({content: self.description});
+	self.marker = new google.maps.Marker({
+		position: self.location,
+		title: self.title(),
+		animation: google.maps.Animation.DROP
+	});
+	self.marker.setMap(map);
+	self.marker.addListener('click', function() {
+		self.infoWindow.open(map, this);
+	});
 };
 
 //-------------- ViewModel ---------------------------------------------------//
 var AppViewModel= function() {
 	var self = this;
 	
-	this.placesList = ko.observableArray([]);
+	self.placesList = ko.observableArray([]);
 	places.forEach(function(place){
-		self.placesList.push(new Marker(place));
-		});
-
+		self.placesList.push(new Place(place));
+	});
 }
 
-// Activate knockout.js
-ko.applyBindings(new AppViewModel());
+
+//Initialize the map - this is called in callback after googlemaps api link
+var init_lat_lng = {lat: 55.9533, lng: -3.1833};
+function initMap  () {
+	console.log('Google maps API call completed');
+
+	var markers = [];
+
+	map = new google.maps.Map(document.getElementById('map'),
+		{center: init_lat_lng,
+		zoom: 10,
+		styles: styles
+		}
+	);
+	var bounds = new google.maps.LatLngBounds();
+	
+	for (var i=0; i< places.length; i++){
+		var location=places[i].location;
+		var title=places[i].title;
+		//var marker = new google.maps.Marker({
+		//	map: map,
+		//	position: location,
+		//	title: title,
+		//	animation: google.maps.Animation.DROP,
+		//	icon: defaultIcon
+		//});
+		//markers.push(marker);
+		//bounds.extend(markers[i].position);
+		//marker.addListener('click', function (){
+			//populateInfoWindow(this, infoWindow);
+		//});
+		
+		// Two event listeners - one for mouseover, one for mouseout,
+		// to change the colors back and forth.
+	//	marker.addListener('mouseover', function() {
+	//		this.setIcon(highlightedIcon);
+	//	});
+	//	marker.addListener('mouseout', function() {
+	//		this.setIcon(defaultIcon);
+	//	});
+	//
+	//	map.fitBounds(bounds);
+	};
+	// Styled default icon
+	var defaultIcon = makeMarkerIcon('0091ef');
+	
+	// Styled highlight icon for when the user
+	// mouses over the marker.
+	var highlightedIcon = makeMarkerIcon('FFFF24');
+	
+	var infoWindow = new google.maps.InfoWindow();
+	
+	document.getElementById('show-points').addEventListener('click', console.log('click'));
+	document.getElementById('hide-points').addEventListener('click', console.log('click'));
+	
+	// Activate knockout.js
+	ko.applyBindings(new AppViewModel());
+}
+
+function makeMarkerIcon(markerColor) {
+	var markerImage = new google.maps.MarkerImage(
+		'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+ markerColor +
+		'|40|_|%E2%80%A2',
+		new google.maps.Size(21, 34),
+		new google.maps.Point(0, 0),
+		new google.maps.Point(10, 34),
+		new google.maps.Size(21,34)
+	);
+	return markerImage;
+};
+
+function populateInfoWindow(marker, infoWindow) {
+// check to make sure the infoWindow is not already this one.
+	if (infoWindow.marker != marker) {
+		infoWindow.marker = marker;
+		infoWindow.setContent('<div>' + marker.title + '</div>');
+		infoWindow.open(map, marker);
+		// Make sure the marker property is cleared if the infowindow is closed.
+		infoWindow.addListener('closeclick',function(){
+			infoWindow.setMarker = null;
+		});
+	}
+};
+
+//function showPoints() {
+//	var bounds = new google.maps.LatLngBounds();
+//	// Extend the boundaries of the map for each marker and display the marker
+//	for (var i = 0; i < markers.length; i++) {
+//		markers[i].setMap(map);
+//		bounds.extend(markers[i].position);
+//	}
+//	map.fitBounds(bounds);
+//};
+
+//function hidePoints() {
+//	for (var i = 0; i < markers.length; i++) {
+//		markers[i].setMap(null);
+//	}
+//};
+var styles=[
+// Styling object for Googlemaps
+// Go to snazzymaps.com at end of Project to custom style.
+	{
+		"featureType": "administrative",
+		"elementType": "all",
+		"stylers": [
+			{
+				"visibility": "on"
+			}
+		]
+	},{
+		"featureType": "administrative",
+		"elementType": "labels.text.fill",
+		"stylers": [
+			{
+				"color": "#444444"
+			}
+		]
+	},{
+		"featureType": "administrative.country",
+		"elementType": "geometry.fill",
+		"stylers": [
+			{
+				"lightness": "-63"
+			}
+		]
+	},{
+		"featureType": "administrative.province",
+		"elementType": "labels",
+		"stylers": [
+			{
+				"visibility": "off"
+			}
+		]
+	},{
+		"featureType": "administrative.locality",
+		"elementType": "labels",
+		"stylers": [
+			{
+				"visibility": "on"
+			},
+			{
+				"color": "#ff7200"
+			},
+			{
+				"weight": "0.64"
+			}
+		]
+	},{
+		"featureType": "administrative.land_parcel",
+		"elementType": "geometry",
+		"stylers": [
+			{
+				"saturation": "-47"
+			},
+			{
+				"lightness": "-22"
+			},
+			{
+				"gamma": "4.71"
+			},
+			{
+				"weight": "5.04"
+			},
+			{
+				"visibility": "on"
+			},
+			{
+				"color": "#d21f1f"
+			}
+		]
+	},{
+		"featureType": "landscape",
+		"elementType": "all",
+		"stylers": [
+			{
+				"color": "#f2f2f2"
+			}
+		]
+	},{
+		"featureType": "poi",
+		"elementType": "all",
+		"stylers": [
+			{
+				"visibility": "off"
+			}
+		]
+	},{
+		"featureType": "road",
+		"elementType": "all",
+		"stylers": [
+			{
+				"saturation": -100
+			},
+			{
+				"lightness": 45
+			}
+		]
+	},{
+		"featureType": "road.highway",
+		"elementType": "all",
+		"stylers": [
+			{
+				"visibility": "simplified"
+			}
+		]
+	},{
+		"featureType": "road.highway",
+		"elementType": "geometry.fill",
+		"stylers": [
+			{
+				"visibility": "on"
+			},
+			{
+				"color": "#ffffff"
+			}
+		]
+	},{
+		"featureType": "road.highway",
+		"elementType": "geometry.stroke",
+		"stylers": [
+			{
+				"color": "#be2727"
+			}
+		]
+	},{
+		"featureType": "road.arterial",
+		"elementType": "labels.icon",
+		"stylers": [
+			{
+				"visibility": "off"
+			}
+		]
+	},{
+		"featureType": "transit",
+		"elementType": "all",
+		"stylers": [
+			{
+				"visibility": "off"
+			}
+		]
+	},{
+		"featureType": "water",
+		"elementType": "all",
+		"stylers": [
+			{
+				"color": "#46bcec"
+			},
+			{
+				"visibility": "on"
+			}
+		]
+	},{
+		"featureType": "water",
+		"elementType": "geometry.fill",
+		"stylers": [
+			{
+				"color": "#003781"
+			}
+		]
+	}
+];

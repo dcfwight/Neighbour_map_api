ef6ce6484a14fe3ce781b90bfa2dfe00ddce5d59
Doug Wight <dcfwight@gmail.com>
2017-11-10 14:12:09 +0000

    Add list of places with place_id

--- js/app.js
+++ js/app.js
@@ -2,6 +2,17 @@ var $test=$('#test')
 
 $test.text('Success: changed via jQuery')
 
+var places = [
+	{name: "Edinburgh Castle",
+	place_id: "ChIJ98CZIJrHh0gRWApM5esemkY"},
+	
+	{name: "Arthur's Seat",
+	place_id: "ChIJuSIvEYS4h0gR8EyTQWxGiiE"
+		
+	}
+]
+
+
 //-------------- ViewModel ---------------------------------------------------//
 function AppViewModel() {
 	this.testName = ko.observable('Success: changed via knockout');

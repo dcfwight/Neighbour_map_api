2bcc3e42aa9c5bdf27d3381d2e53bc8fc3d1dd73
Doug Wight <dcfwight@gmail.com>
2017-11-10 14:39:16 +0000

    Create knockout array of places

--- js/app.js
+++ js/app.js
@@ -2,6 +2,9 @@ var $test=$('#test')
 
 $test.text('Success: changed via jQuery')
 
+
+
+
 var places = [
 	{name: "Edinburgh Castle",
 	place_id: "ChIJ98CZIJrHh0gRWApM5esemkY"},
@@ -12,10 +15,22 @@ var places = [
 	}
 ]
 
+var Marker = function(data) {
+	this.name=ko.observable(data.name);
+	this.place_id=ko.observable(data.place_id);
+};
 
 //-------------- ViewModel ---------------------------------------------------//
-function AppViewModel() {
-	this.testName = ko.observable('Success: changed via knockout');
+var AppViewModel= function() {
+	var self = this;
+	
+	this.testName = ko.observable('Succes - changed via knockout')
+	
+	this.placeList = ko.observableArray([]);
+	places.forEach(function(place){
+		self.placeList.push(new Marker(place));
+		});
+
 }
 
 // Activate knockout.js

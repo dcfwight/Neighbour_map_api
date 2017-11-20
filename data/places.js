cabde3f7129dab198565a2cf1b9d6ff65d3718a8
Doug Wight <dcfwight@gmail.com>
2017-11-20 17:57:20 +0000

    Add data/places.js file

---
+++ data/places.js
@@ -0,0 +1,49 @@
+var places = [
+		{
+		"title": "Loch Fyne Seafood",
+		"location": {
+			"lat": 55.981120,
+			"lng": -3.194866
+		},
+		"type": 'restaurant',
+		"fourSqID": '4b214c89f964a520c93924e3',
+		"gPlaceID": 'ChIJP4Id2PzHh0gRxyf9JTfA2Mw'	
+	}, {
+		"title": "the Dogs",
+		"location": {
+			"lat":55.9545302,
+			"lng":-3.1980785
+		},
+		"type": 'restaurant',
+		"fourSqID": '4b06ce8df964a52095f022e3',
+		"gPlaceID": 'ChIJE7uoQZHHh0gRlH_vh2FS9pc'
+		
+	}, {
+		"title": "Yo sushi!",
+		"location": {
+			"lat":55.9521785,
+			"lng":-3.1966648
+		},
+		"type": 'restaurant',
+		"fourSqID": '5480516f498e6462fccfb8a8',
+		"gPlaceID": 'ChIJD-4k75DHh0gR-FWBESyD77s'
+	}, {
+		"title": "The Witchery",
+		"location": {
+			"lat": 55.948789,
+			"lng": -3.195628
+		},
+		"type": 'restaurant',
+		"fourSqID": '4bce2e1eef109521b1aa8386',
+		"gPlaceID": 'ChIJNfElXprHh0gR61-eo29tIRs'
+	}, {
+		"title": "The Kitchin",
+		"location": {
+			"lat": 55.97703809999999,
+			"lng": -3.1726892
+		},
+		"type": 'restaurant',
+		"fourSqID": '4b59ff2bf964a5209ca628e3',
+		"gPlaceID": 'ChIJLy3eIwS4h0gRe4oubEitnNQ'
+	}
+];
